fc44fdb2a1b69013db9a5a6158162cd1208426ea
fjcorona <fjavier.corona@hotmail.com>
2021-04-27 23:06:45 -0500

    Functional Interface

---
+++ src/main/java/com/fjcorona/java8/FunctionalInterface/FunctionalInterface.java
@@ -0,0 +1,14 @@
+package com.fjcorona.java8.FunctionalInterface;
+
+public class FunctionalInterface {
+
+    public double opera(double n1, double n2) {
+        Operacion operacion = (x, y) -> x + y;
+        return operacion.calcula(n1, n2);
+    }
+
+    public static void main (String[] args) {
+        FunctionalInterface app = new FunctionalInterface();
+        System.out.println(app.opera(2, 3));
+    }
+}
---
+++ src/main/java/com/fjcorona/java8/FunctionalInterface/Operacion.java
@@ -0,0 +1,9 @@
+package com.fjcorona.java8.FunctionalInterface;
+import java.lang.FunctionalInterface;
+
+@FunctionalInterface
+public interface Operacion {
+
+    public double calcula(double n1, double n2);
+
+}
