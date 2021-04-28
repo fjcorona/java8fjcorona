c48c1e0b6e69ce3755056fb3396b3092d42306c0
fjcorona <fjavier.corona@hotmail.com>
2021-04-28 11:01:22 -0500

    Change variable names from functional interface

--- src/main/java/com/fjcorona/java8/FunctionalInterface/FunctionalInterface.java
+++ src/main/java/com/fjcorona/java8/FunctionalInterface/FunctionalInterface.java
@@ -3,8 +3,8 @@ package com.fjcorona.java8.FunctionalInterface;
 public class FunctionalInterface {
 
     public double opera(double n1, double n2) {
-        Operation operacion = (x, y) -> x + y;
-        return operacion.calcula(n1, n2);
+        Operation operation = (x, y) -> x + y;
+        return operation.calcula(n1, n2);
     }
 
     public static void main (String[] args) {
