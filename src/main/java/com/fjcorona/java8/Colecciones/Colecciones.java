c599e9162b58f651cac7f4d6e9c0851af5e1f3bd
fjcorona <fjavier.corona@hotmail.com>
2021-04-27 23:12:40 -0500

    Colllections

---
+++ src/main/java/com/fjcorona/java8/Colecciones/Colecciones.java
@@ -0,0 +1,113 @@
+package com.fjcorona.java8.Colecciones;
+
+import java.util.*;
+
+public class Colecciones {
+
+    private List<String> lista;
+
+    public void llenaLista() {
+        lista = new ArrayList<>();
+
+        lista.add("JavoCrown");
+        lista.add("Javo");
+        lista.add("Crown");
+    }
+
+    public void usaForEach() {
+        // Con un ciclo for común y corriente
+
+        /*
+        for (String element : lista) {
+            System.out.println(element);
+        }
+        */
+
+        // Con una expresión lambda
+        // lista.forEach( x -> System.out.println(x) );
+
+        // Con referencia a método
+        lista.forEach(System.out::println);
+    }
+
+    public void usaRemoveIf() {
+        // Con un ciclo for. Retorna una excepción
+        /*
+        for (String element : lista) {
+            if (element.equalsIgnoreCase("Crown")) {
+                lista.remove(element);
+            }
+        }
+        */
+
+        // Con un iterator
+        /*
+        Iterator<String> it = lista.iterator();
+        while (it.hasNext()) {
+            if(it.next().equalsIgnoreCase("Crown")) {
+                it.remove();
+            }
+        }
+        */
+
+        // Con una expresión lambda
+        lista.removeIf(x -> x.equalsIgnoreCase("Crown"));
+    }
+
+    public void usaSort() {
+
+
+        // -------- CON ḾETODO SORT DE COLLECTIONS
+
+
+        // Clase anónima
+        /*
+        Collections.sort(lista, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.compareToIgnoreCase(o2);
+            }
+        });
+        */
+
+        // Expresión lambda
+        // Collections.sort(lista, (x, y) -> x.compareToIgnoreCase(y));
+
+        // Referencia a método
+        // Collections.sort(lista, String::compareToIgnoreCase);
+
+
+        // -------- CON ḾETODO SORT PROPIO DE LA LISTA
+
+
+        // Clase anónima
+        /*
+        lista.sort(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.compareToIgnoreCase(o2);
+            }
+        });
+        */
+
+        // Expresión lambda
+        // lista.sort( (x, y) -> x.compareToIgnoreCase(y) );
+
+        // Referencia a método
+        lista.sort(String::compareToIgnoreCase);
+    }
+
+    public static void main (String[] args) {
+        Colecciones app = new Colecciones();
+
+        app.llenaLista();
+        app.usaForEach();
+
+        app.usaSort();
+        app.usaForEach();
+
+        app.usaRemoveIf();
+        app.usaForEach();
+    }
+
+}
